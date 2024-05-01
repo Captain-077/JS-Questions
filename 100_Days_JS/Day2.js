ca08edc75371533e8c4306c6a4ac839b01744706
Your Name <nakuldesigner77@gmail.com>
2024-05-02 00:59:00 +0530

    Day2 of js topic: hash generator function

---
+++ 100_Days_JS/Day2.js
@@ -0,0 +1,22 @@
+//Q? You are required to implement a function generateHash that generates a hash tag from a given input string. The hash tag should be constructed as follows:
+
+
+const generateHash = (str) => {
+    if(str.length > 280 || str.length ===0){
+        return false 
+    }
+    
+   str = str.split(" ");
+   str =  str.map((item) => {
+            return item.replace(item[0],item[0].toUpperCase())
+   })
+    
+    str = `#${str.join("")}`;
+    
+    // console.log(str)
+    return str
+   
+    
+};
+
+console.log(generateHash("my name is slim shady"));
